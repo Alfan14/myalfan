be35c2ee919ca976374070afda662b1239a276f2
Alfan <138642451+Alfan14@users.noreply.github.com>
2025-12-02 19:43:27 +0700

    Update edit.php

--- udddp/edit.php
+++ udddp/edit.php
@@ -6,7 +6,7 @@
   $alamat = $_POST[‘alamat’]
   $kelas = $_POST[‘kelas’]
 
-  mysql_query(“UPDATE mahasiswa SET nama=’$nama’,  alamat=’$alamat’, kelas=’$kelas’ WHERE npm=’$npm’”);
+  mysql_query("UPDATE mahasiswa SET nama=’$nama’,  alamat=’$alamat’, kelas=’$kelas’ WHERE npm=’$npm’");
 
   header(“location:index.php?pesan=update”);
 ?>
